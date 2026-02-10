20bac03f730bc334424414c3872c544cfa228c7f
Josh Ben-Nathan <jmb-n@outlook.com>
2026-02-10 23:46:03 +0100

    Final files for 1.2

--- main.cpp
+++ main.cpp
@@ -3,16 +3,57 @@
 // Group : 8
 // License : LGPL open source license
 //
-// Brief : Assignment 1.1
+// Brief : Assignment 1.2
 //=====================...........................=================================
 
-int divide(const int num, const int den);
-
 #include <iostream>
+#include <cmath>
+
+double under = 18.5;
+double normal = 25;
+double over = 30;
+
+double divide(const double num, const double den) {
+    return num/pow(den, 2);
+}
+
+void printInfo() {
+    std :: cout << "BMI VALUES:\nUnderweight: less than " << under << std::endl
+    << "Normal: between " << under << " and " << normal << std::endl
+    << "Overweight: between " << normal << " and " << over << std::endl
+    << "Obese: " << over << " or greater" << std::endl;
+}
+
+void evaluateAndPrintBMI (const double bmi){
+    std :: cout << "Your BMI is: " << bmi << "\n" << "You are ";
+    if (bmi < under) {
+        std :: cout << "underweight" << std::endl;
+    }
+    else if (bmi < normal) {
+        std :: cout << "normal weight" << std::endl;
+    }
+    else if (bmi < over) {
+        std :: cout << "overweight" << std::endl;
+    }
+    else {
+        std :: cout << "obese" << std::endl;
+    }
+}
+
 int main(void) {
-    int a = 7;
-    int b = 3;
-    int c = divide(a, b);
-    std :: cout << a << "/" << b << "=" << c << std::endl;
+    double weight, height;
+
+    printInfo();
+
+    std::cout << "How wide are you (kg): ";
+    std::cin >> weight;
+
+    std::cout << "How long are you (m): ";
+    std::cin >> height;
+
+    double bmi = divide(weight, height);
+
+    evaluateAndPrintBMI(bmi);
+
     return 0;
 }
