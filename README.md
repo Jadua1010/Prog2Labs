0da84254426b492d9aec3d4099e7943228c78d02
Josh Ben-Nathan <jmb-n@outlook.com>
2026-03-03 12:23:19 +0100

    Included all juicy 3.2 files

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-Hello welcome to our assignment x
+Hello welcome to our assignment 3
 ---------------------------------
 Please build the code using the following:
-g++ src/main.cpp −I ./include −o app.exe
+g++ src/main.cpp -I include −o app.exe
 
 And then of course run with:
 ./app.exe
--- src/test.cpp
+++ src/test.cpp
@@ -13,10 +13,10 @@
 int main() {
     Tree<int> t;
 
-    std::vector<int> fillers = {10, 45, 1, 6, 7, 90, 43, 77, 84, 35};
+    std::vector<int> fillers = {10, 45, 1, 6, 7, 90, 43, 77, 84, 35, 44, 67};
 
     // Now go through all filler values and insert them into a tree
-    std::cout << "Inserting 10 fun integers" << std::endl;
+    std::cout << "TEST: Inserting 12 fun integers (instead of 10)" << std::endl;
     for (int val : fillers) {
         t.insertNode(val);
     }
@@ -27,7 +27,7 @@ int main() {
     // Setup search value
     int searchVal = 0;
     std::cout << "Enter a value you'd like to find:" << std::endl;
-    std::cin >> searchVal;
+    std::cout << "TEST: What happens if we enter something random that is not an int (equivalent to nothing)" << std::endl;
 
     // Warn the user if the searchval did not get updated
     if (searchVal == 0)
