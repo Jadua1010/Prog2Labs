ca0695cfb3a51203240572c1dce74abcc24f6ea6
Josh Ben-Nathan <jmb-n@outlook.com>
2026-03-16 22:47:46 +0100

    Setup SDL2 files

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Hello welcome to our assignment x
 ---------------------------------
 
 Please build the code using the following:
-g++ src/main.cpp -I include -o app.exe
+g++ src/*.cpp -I include -I include/SDL2/include -L include/SDL2/lib -lmingw32 -lSDL2main -lSDL2 -o complexity.exe
 
 And then of course run with:
 ./app.exe
---
+++ src/DrawContourScanning.cpp
@@ -0,0 +1,17 @@
+//==============================================================
+// Filename    : 
+// Authors     : 
+// Group       :
+// License     :  N.A. or opensource license like LGPL
+// Description : 
+//==============================================================
+
+#include "DrawContourScanning.h" 
+
+// draw contour
+void DrawContourScanning::drawContour(float threshold) {
+
+    // YOUR CODE HERE
+
+    ui->drawPixel(0, 0); // Dummy code only
+}
